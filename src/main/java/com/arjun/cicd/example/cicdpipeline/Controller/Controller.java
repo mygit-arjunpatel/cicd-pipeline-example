1e554c0b1158e54dbcb235c6034eea02ed14732d
Arjun Patel <arjunpatel@Arjuns-MacBook-Pro.local>
2020-08-16 16:32:03 -0400

    updated java version

--- src/main/java/com/arjun/cicd/example/cicdpipeline/Controller/Controller.java
+++ src/main/java/com/arjun/cicd/example/cicdpipeline/Controller/Controller.java
@@ -1,2 +1,16 @@
-package com.arjun.cicd.example.cicdpipeline.Controller;public class Controller {
+package com.arjun.cicd.example.cicdpipeline.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/App")
+public class Controller {
+
+    @GetMapping(value = "/GetStatus", consumes = "application/json", produces = "application/json")
+    public String GetStatus()
+    {
+        return "We are into the GetStatus Controller Get mapping";
+    }
 }
--- src/test/java/com/arjun/cicd/example/cicdpipeline/CicdPipelineApplicationTests.java
+++ src/test/java/com/arjun/cicd/example/cicdpipeline/CicdPipelineApplicationTests.java
@@ -3,11 +3,21 @@ package com.arjun.cicd.example.cicdpipeline;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 @SpringBootTest
 class CicdPipelineApplicationTests {
 
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	public void TestDemo(){
+		String actual = "Actual test";
+		String expected = "Actual test";
+		assertEquals(actual,expected);
+
 	}
 
 }
