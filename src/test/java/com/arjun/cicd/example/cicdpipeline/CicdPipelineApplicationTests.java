e5a53836e68d8a7c55d5214bcc124f67cae17ed4
Arjun Patel <arjunpatel@Arjuns-MacBook-Pro.local>
2020-08-16 15:11:35 -0400

    initial version

---
+++ src/test/java/com/arjun/cicd/example/cicdpipeline/CicdPipelineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arjun.cicd.example.cicdpipeline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CicdPipelineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
